4a061507e61feae707a65c0289cd610ea72b5609
Reymundo Tenorio <Reymundo Tenorio>
2016-12-08 20:31:25 -0600

    Se ha modificado README

--- src/Clases/Conexion.java
+++ src/Clases/Conexion.java
@@ -4,20 +4,19 @@ import Principal.IniciarSesion;
 import java.sql.*;
 import javax.swing.JOptionPane;
 
-
 public class Conexion {
-public static Connection con;
-public static Statement state,state1;
-public static ResultSet result,result1;
 
-public void Conectar(String url,String user,String pass)throws SQLException,ClassNotFoundException
-{  
-    
-    try {
+    public static Connection con;
+    public static Statement state, state1;
+    public static ResultSet result, result1;
+
+    public void Conectar(String url, String user, String pass) throws SQLException, ClassNotFoundException {
+
+        try {
             // Get connection
-        
+
             //Class.forName("com.mysql.jdbc.Driver");   
-            DriverManager.registerDriver( new com.mysql.jdbc.Driver());
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
             con = DriverManager.getConnection("jdbc:mysql://localhost/Revista", user, pass);
 
 //            if (con!= null) {
@@ -31,38 +30,33 @@ public void Conectar(String url,String user,String pass)throws SQLException,Clas
 //                System.out.println("Database Name: " + meta.getDatabaseProductName());
 //                System.out.println("Database Version: " + meta.getDatabaseProductVersion());
 //            }
-            
-           
-
- //   Class.forName("sun.jdbc.odb.JdbcOdbcDriver");   
-   // Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-  //con=DriverManager.getConnection("jdbc:odbc:Driver={SQL Server};Server=REYMUNDOASUS"
-     //   + "; Database=Comedor;Uid="+user+"; Pwd="+pass); //Conexion Directa
+            //   Class.forName("sun.jdbc.odb.JdbcOdbcDriver");   
+            // Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+            //con=DriverManager.getConnection("jdbc:odbc:Driver={SQL Server};Server=REYMUNDOASUS"
+            //   + "; Database=Comedor;Uid="+user+"; Pwd="+pass); //Conexion Directa
 // con=DriverManager.getConnection(url,user,pass); // Conexion ODBC
-    
-    
-    state=con.createStatement(result.TYPE_SCROLL_SENSITIVE,result.CONCUR_UPDATABLE);
-    
-    JOptionPane.showMessageDialog(null,"Conexión Establecida", "Conexión Establecida",JOptionPane.INFORMATION_MESSAGE);
+            state = con.createStatement(result.TYPE_SCROLL_SENSITIVE, result.CONCUR_UPDATABLE);
 
-      } catch (SQLException se) {
-          //  JOptionPane.showMessageDialog(null,se.getMessage(),"Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Conexión Establecida", "Conexión Establecida", JOptionPane.INFORMATION_MESSAGE);
+
+        } catch (SQLException se) {
+            //  JOptionPane.showMessageDialog(null,se.getMessage(),"Error", JOptionPane.ERROR_MESSAGE);
             this.Ini.ErrorConexion();
-             
-        }
-}//fin del constructor
- public static ResultSet consulta(String sql)throws SQLException{
 
-        state1 = (Statement) con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,result1.CONCUR_READ_ONLY);
+        }
+    }//fin del constructor
+
+    public static ResultSet consulta(String sql) throws SQLException {
+
+        state1 = (Statement) con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, result1.CONCUR_READ_ONLY);
         result1 = state1.executeQuery(sql);//aqui hago consultas y devuelvo los resultados
         return result1;
-}
- 
- private IniciarSesion Ini;
+    }
+
+    private IniciarSesion Ini;
 
     public void setIni(IniciarSesion Ini) {
         this.Ini = Ini;
     }
-         
- 
-         }
+
+}
